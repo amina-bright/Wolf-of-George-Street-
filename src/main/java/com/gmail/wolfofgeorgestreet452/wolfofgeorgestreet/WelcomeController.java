a7b491cefb19c660cc4c893711d334ed44fc4786
KyleThakker <kthak5k5@gmail.com>
2018-02-17 18:08:55 -0500

    -Now renders jsp files

--- src/main/java/com/gmail/wolfofgeorgestreet452/wolfofgeorgestreet/WelcomeController.java
+++ src/main/java/com/gmail/wolfofgeorgestreet452/wolfofgeorgestreet/WelcomeController.java
@@ -1,19 +1,17 @@
 package com.gmail.wolfofgeorgestreet452.wolfofgeorgestreet;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.Map;
 
-@RestController
+@Controller
 public class WelcomeController {
-    @Value("${welcome.message:test}")
-    private String message = "Hello World";
 
-    @RequestMapping("/")
-    public String welcome(Map<String, Object> model) {
-        model.put("message", this.message);
+    @GetMapping("/")
+    public String hello(Model model, @RequestParam(value="name", required=false, defaultValue="World") String name) {
+        model.addAttribute("name", name);
         return "welcome";
     }
 }
