ff46dd5abf9d9d131141c7b106823f3bdf695ffd
KyleThakker <kthak5k5@gmail.com>
2018-02-17 00:44:21 -0500

    Set up a lot of the sturcture

---
+++ src/main/java/com/gmail/wolfofgeorgestreet452/wolfofgeorgestreet/WolfofgeorgestreetApplication.java
@@ -0,0 +1,12 @@
+package com.gmail.wolfofgeorgestreet452.wolfofgeorgestreet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WolfofgeorgestreetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WolfofgeorgestreetApplication.class, args);
+	}
+}
