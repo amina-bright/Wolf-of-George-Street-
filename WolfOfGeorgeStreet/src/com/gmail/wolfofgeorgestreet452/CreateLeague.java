1331f182380da70d68e335da6da3796b9103c0d9
TaewonSoftwareEngS2018 <36053035+TaewonSoftwareEngS2018@users.noreply.github.com>
2018-03-27 19:45:06 -0400

    updated java file for create league

--- WolfOfGeorgeStreet/src/com/gmail/wolfofgeorgestreet452/CreateLeague.java
+++ WolfOfGeorgeStreet/src/com/gmail/wolfofgeorgestreet452/CreateLeague.java
@@ -7,10 +7,12 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import java.sql.*;
-import java.util.ArrayList;
 
+import java.util.ArrayList;
+//import java.util.Date;
 import com.mysql.jdbc.Driver;
 
 /**
@@ -18,7 +20,7 @@ import com.mysql.jdbc.Driver;
  */
 @WebServlet("/createleague")
 public class CreateLeague extends HttpServlet {
-	
+	private static final long serialVersionUID = 1L;
 	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
 	static final String DB_URL = "jdbc:mysql://wolfofgeorgestreet.c984b9paepnh.us-east-2.rds.amazonaws.com:3306/WolfOfGeorgeStreetDB";
     
@@ -45,8 +47,82 @@ public class CreateLeague extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		return;
+protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	    //String username = request.getSession().getAttribute("username");
+		String button=request.getParameter("Submit");
+		
+		
+		String leagueName = request.getParameter("leagueName");
+		int gameMode = Integer.parseInt(request.getParameter("gameMode"));
+		java.util.Date startDate = Date.valueOf(request.getParameter("startDate"));
+		java.util.Date endDate = Date.valueOf(request.getParameter("endDate"));
+		int crypto = Integer.parseInt(request.getParameter("crypto"));
+		int maxParticipantNum = Integer.parseInt(request.getParameter("maxParticipantNum"));
+		double startCapital = Double.parseDouble(request.getParameter("startCapital"));
+		
+		java.sql.Date startDateSQL = new java.sql.Date(startDate.getTime());
+		java.sql.Date endDateSQL = new java.sql.Date(endDate.getTime());
+		int leagueID = (int)(Math.random()*999999+100000); //generate random number for leagueID
+		
+		
+		//if the submit button was clicked
+		if("submit".equals(button)) {			
+			 Connection conn = null;
+			 PreparedStatement stmt = null;
+			 
+			 try{
+			      //register jdbc driver
+			      Class.forName("com.mysql.jdbc.Driver");
+
+			      //open a connection
+			      conn = DriverManager.getConnection(DB_URL,USER,PASS);
+
+			      
+			      String sql;
+			      //add the league to database
+			      sql = "INSERT INTO League (leagueID, leagueName, size, username, startDate, endDate, crypto, startingPrinciple, gameMode)"
+				      		+ "VALUES ('" + leagueID + "', '" + leagueName + "', '" + maxParticipantNum + "', '" + request.getSession().getAttribute("username") + "', '" + startDateSQL + 
+				      		"','" + endDateSQL + "', b'" + crypto + "', '" + startCapital + "', b'" + gameMode + "')";
+			      
+			      stmt=conn.prepareStatement(sql);
+			      stmt.executeUpdate();
+			      
+			      //add host to the list of users of this league
+			      sql = "INSERT INTO LeagueUserList (username, leagueID, liquidMoney)"
+				      		+ "VALUES ('" + request.getSession().getAttribute("username") + "', '" + leagueID + "', '" + startCapital + "')";
+			      
+			      stmt=conn.prepareStatement(sql);
+			      stmt.executeUpdate();
+
+			      stmt.close();
+			      conn.close();
+			      
+		    	  request.setAttribute("success",true);
+		    	 
+		    	  System.out.println("success");
+		     
+		    	  response.sendRedirect(request.getContextPath()+ "/league");
+			      
+			   }catch(SQLException se){
+			      se.printStackTrace();
+			   }catch(Exception e){
+			      e.printStackTrace();
+			   }finally{
+			      try{
+			         if(stmt!=null)
+			            stmt.close();
+			      }catch(SQLException se2){
+			      }
+			      try{
+			         if(conn!=null)
+			            conn.close();
+			      }catch(SQLException se){
+			         se.printStackTrace();
+			      }
+			   }	
+		}//end of if
+		
 	}
 
 }
